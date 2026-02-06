794f482712922a40c0dda20c3a3bbd9f10f35079
Vũ Văn Quân <quancn27@gmail.com>
2026-02-07 00:01:31 +0700

    Chỉnh sửa layout

--- src/app/shared/components/layout/footer/footer.component.ts
+++ src/app/shared/components/layout/footer/footer.component.ts
@@ -17,6 +17,7 @@ export class FooterComponent {
   email = 'Quancn27@gmail.com';
   facebookUrl = 'https://www.facebook.com/hang.nguyen.thi.526333';
   address = 'Số nhà 86 Đường H28, Canh Nậu, Xã Tây Phương, Thành phố Hà Nội';
+  workingHours = '8:00 - 18:00';
   mapUrl = 'https://www.google.com/maps/place/3J57%2B227,+Lam+S%C6%A1n,+Th%E1%BA%A1ch+Th%E1%BA%A5t,+H%C3%A0+N%E1%BB%99i,+Vi%E1%BB%87t+Nam/@21.0573829,105.6127367,19z/data=!4m9!1m2!2m1!1zM2o1NysyMjcgY2FuaCBu4bqtdSB0aOG6oWNoIHRo4bqldCBow6AgbuG7mWk!3m5!1s0x3134572313105841:0xd23605ef42f2f9ae!8m2!3d21.0575375!4d105.6125469!15sCiwzajU3KzIyNyBjYW5oIG7huq11IHRo4bqhY2ggdGjhuqV0IGjDoCBu4buZaZIBEGdlb2NvZGVkX2FkZHJlc3PgAQA?entry=ttu&g_ep=EgoyMDI2MDIwMy4wIKXMDSoASAFQAw%3D%3D';
   
   // Embed URL với tọa độ: 21.0575375, 105.6125469
